2157eb8fdba860d3780fcd8989f3206c03645c6f
Vladislav <kim2611kim2611@gmail.com>
2026-05-11 19:43:22 +0900

    Fix domain headers

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "ALLOWALL",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
